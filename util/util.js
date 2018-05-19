d19feb0e67d143715be20c1f1d1bf01c9f6a7992
谢乾 <qian_xie@mytophome.com>
2018-05-19 16:18:56 +0800

    获取form表单值

--- util/util.js
+++ util/util.js
@@ -8,6 +8,7 @@
     }
 })(this, function () {
     var util = {};
+    // 获取查询字符串中的变量值
     util.queryString = function (name) {
         var result = location.search.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
         if (result == null || result.length < 1) {
@@ -31,6 +32,52 @@
         return arr;
     };
 
+    // 获取表单数据
+    util.getSerializeData = (id) => {
+        if (typeof id !== "string") return {};
+        var pElt = document.getElementById(id);
+        if (pElt === null) {
+            return {};
+        }
+
+        var result = {};
+        var item;
+        var inputList = pElt.getElementsByTagName('input');
+        for (var i = 0, len = inputList.length; i < len; i++) {
+            item = inputList[i];
+            // 有name值才需取值
+            if (item.name !== "") {
+                switch (item.type) {
+                    case "radio":
+                        if (result[item.name] === '' || result[item.name] === undefined) {
+                            result[item.name] = item.checked ? item.value : "";
+                        }
+                        break;
+                    case "checkbox":
+                        result[item.name] = result[item.name] || [];
+                        if (item.checked) {
+                            result[item.name].push(item.value);
+                        }
+                        break;
+                    default:
+                        result[item.name] = item.value.trim();
+                }
+                item = null;
+            }
+        }
+
+        var textareaList = pElt.getElementsByTagName('textarea');
+        for (i = 0, len = textareaList.length; i < len; i++) {
+            item = textareaList[i];
+            // 有name值才需取值
+            if (item.name !== "") {
+                result[item.name] = item.value.trim();
+                item = null;
+            }
+        }
+
+        return result;
+    };
 
     return util;
 });
